8dc7d1e054ba2e06a4855487d894e73f54375b17
Maxime <michet.maxime@gmail.com>
2020-07-30 14:29:52 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom'
-import {auth} from './Auth'
 //Pages
 import MainPage from './pages/index'
 import NotFoundPage from './pages/404'
@@ -13,10 +12,9 @@ import Main from './pages/main'
 function App() {
   return (
   <Router>
-    <auth/>
     <Switch>
     <Route exact path="/" component={MainPage}/>
-    <Route exact path="/404" component={NotFoundPage}/>
+    {/* <Route exact path="/404" component={NotFoundPage}/> */}
     <Route exact path="/main" component={Main}/>
     
     <Redirect to="/404"/>
