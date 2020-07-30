8504673dc1a0aea783c07d6469fd8f91342e66e5
Maxime <michet.maxime@gmail.com>
2020-07-30 13:46:43 -0400

    Updates

---
+++ precache-manifest.12f849f409d471549dc4045064c16169.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7834ef8e5dfd17812e4f8cfe3f60406",
+    "url": "/Task-App-Front-End/index.html"
+  },
+  {
+    "revision": "20ed18f82b84188cf9f8",
+    "url": "/Task-App-Front-End/static/css/2.e37b16eb.chunk.css"
+  },
+  {
+    "revision": "6947b6dc6f39a9ff47f0",
+    "url": "/Task-App-Front-End/static/css/main.dd1e8771.chunk.css"
+  },
+  {
+    "revision": "20ed18f82b84188cf9f8",
+    "url": "/Task-App-Front-End/static/js/2.51b8bbd1.chunk.js"
+  },
+  {
+    "revision": "314484c64aa189fcf7dfd469baf98204",
+    "url": "/Task-App-Front-End/static/js/2.51b8bbd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6947b6dc6f39a9ff47f0",
+    "url": "/Task-App-Front-End/static/js/main.0b50bf49.chunk.js"
+  },
+  {
+    "revision": "f583a8d69d6aadc61547",
+    "url": "/Task-App-Front-End/static/js/runtime-main.d02ba498.js"
+  },
+  {
+    "revision": "34bb4d3994260af4f450f4050837142a",
+    "url": "/Task-App-Front-End/static/media/checkmark.34bb4d39.png"
+  },
+  {
+    "revision": "8ecdfd685ecea96038fdd13c0bdab3bc",
+    "url": "/Task-App-Front-End/static/media/cross.8ecdfd68.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Task-App-Front-End/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a7f9007647dcec4ee57ac9c8048c7d24",
+    "url": "/Task-App-Front-End/static/media/tasks.a7f90076.svg"
+  }
+]);
